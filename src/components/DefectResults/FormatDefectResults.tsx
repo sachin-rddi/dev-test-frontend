88e19bc7504725cab276139bd1db9d526a38e0be
Sachin Dias RDDI <sachin@edflabs.net>
2026-03-13 13:28:26 +0000

    simplified return

--- src/components/DefectResults/FormatDefectResults.tsx
+++ src/components/DefectResults/FormatDefectResults.tsx
@@ -10,29 +10,28 @@ export interface Defect {
 }
 
 const FormatDefectResults = ({ results }: { results: Defect[] | null }) => {
-  if (!results || results.length === 0) {
-    return <h3>No defects detected.</h3>;
-  } else {
-    return (
-      <div className={styles.defectResultsContainer}>
-        <h3>{results.length} Defects Detected:</h3>
-        <ol>
-          {results.map((defect, index) => (
-            <li key={index}>
-              Type: <span className={styles.defectInfo}>{defect.type}</span>
-              <br />
-              Location: (<span className={styles.defectInfo}>{defect.x}</span>,
-              <span className={styles.defectInfo}>{defect.y}</span>)<br />
-              Width:
-              <span className={styles.defectInfo}>{defect.width}</span>
-              <br /> Height:{" "}
-              <span className={styles.defectInfo}>{defect.height}</span>
-            </li>
-          ))}
-        </ol>
-      </div>
-    );
-  }
+  if (!results || results.length === 0) return <h3>No defects detected.</h3>;
+
+  return (
+    <div className={styles.defectResultsContainer}>
+      <h3>{results.length} Defects Detected:</h3>
+      <ol>
+        {results.map((defect, index) => (
+          <li key={index}>
+            Type: <span className={styles.defectInfo}>{defect.type}</span>
+            <br />
+            Location: (<span className={styles.defectInfo}>
+              {defect.x}
+            </span>, <span className={styles.defectInfo}>{defect.y}</span>)
+            <br />
+            Width: <span className={styles.defectInfo}>{defect.width}</span>
+            <br />
+            Height: <span className={styles.defectInfo}>{defect.height}</span>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default FormatDefectResults;
